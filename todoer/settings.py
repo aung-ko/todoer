b5c770c13be2d520dc6e56c6f4f59a80501f352d
Aung Ko Ko <aungkoko174@gmail.com>
2021-05-16 15:53:45 +0630

    removed secret key

--- todoer/settings.py
+++ todoer/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!SECRET_KEY = 'django-insecure-p0jkb=nwv_hyv(v5)oe9@3$hz-s^osj6f@9gyirk4o#l^lbcoz'
 SECRET_KEY = os.environ['TODOER_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
